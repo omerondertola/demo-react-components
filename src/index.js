c438d8362a625f3bf12f0de8ae34b43f5b92d1b7
omerondertola <omerondertola@gmail.com>
2021-02-16 18:09:11 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Heading() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
